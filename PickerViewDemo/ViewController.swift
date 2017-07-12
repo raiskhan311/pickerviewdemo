a9f3efa42c7d265ab6b4be7d48c16bbef823c957
SudhakarGVVN <sudhakar@zanjo.io>
2017-07-12 22:34:54 +0530

    picker view demo

--- PickerViewDemo/ViewController.swift
+++ PickerViewDemo/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet weak var lbl_mood: UILabel!
+    
+    @IBOutlet weak var view_container: UIView!
+    
+    let moodArray = ["Happy","Sad","Maudlin","Ecstatic","Overjoyed","Optimistic","Bewildered","Cynical","Giddy","Indifferent","Relaxed"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view_container.alpha = 0
+        lbl_mood.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btn_mood(_ sender: Any) {
+        view_container.alpha = 1
+    }
+        
+}
 
-
+extension ViewController: UIPickerViewDataSource,UIPickerViewDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return moodArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return moodArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lbl_mood.alpha = 1
+        lbl_mood.text = moodArray[row]
+        view_container.alpha = 0
+    }
 }
 
